d81e46ad789e55dc3556546ffbd20fe66335f607
Bogdan <bogdan.hiriscau@gmail.com>
2019-10-10 09:34:46 +0300

    try 2

--- App.js
+++ App.js
@@ -45,27 +45,6 @@ class Home extends React.Component {
 constructor(properties) {
     super(properties);
     OneSignal.init("5c4c1a85-9f5e-40d1-b915-59eebf374c29");
-
-    OneSignal.addEventListener('received', this.onReceived);
-    OneSignal.addEventListener('opened', this.onOpened);
-    OneSignal.addEventListener('ids', this.onIds);
-  }
-
-  componentWillUnmount() {
-    OneSignal.removeEventListener('received', this.onReceived);
-    OneSignal.removeEventListener('opened', this.onOpened);
-    OneSignal.removeEventListener('ids', this.onIds);
-  }
-
-  onReceived(notification) {
-    console.log("Notification received: ", notification);
-  }
-
-  onOpened(openResult) {
-    console.log('Message: ', openResult.notification.payload.body);
-    console.log('Data: ', openResult.notification.payload.additionalData);
-    console.log('isActive: ', openResult.notification.isAppInFocus);
-    console.log('openResult: ', openResult);
   }
 
   onIds(device) {
